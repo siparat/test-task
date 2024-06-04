8e65cb267b8994f53c67cfc3c58b165d90e68e57
siparat <akimov1712@yandex.ru>
2024-06-05 00:24:07 +0300

    refactor(app): Added stdout errors

--- src/filters/exception.filter.ts
+++ src/filters/exception.filter.ts
@@ -1,15 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
 import { Exception } from './base.exception';
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
 import 'reflect-metadata';
+import { LoggerService } from '../logger/logger.service';
 
 @injectable()
 export class ExceptionFilter {
-	constructor() {}
+	constructor(@inject(LoggerService) private logger: LoggerService) {}
 
 	catch(err: Error | Exception, req: Request, res: Response, next: NextFunction): void {
 		if (!err) {
 			return next();
+		}
+		if (!(err instanceof Exception)) {
+			this.logger.error(err.stack || err.message);
 		}
 		const status = typeof err == 'object' && 'status' in err ? err.status : 500;
 		const message = typeof err == 'object' && 'message' in err ? err.message : 'Произошла неизвестная ошибка';
